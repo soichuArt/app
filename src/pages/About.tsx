2a6d9b9268066ec2d6512d4f9c7e93fbffcc7d5f
soichuArt <info@soichu.com>
2025-06-30 23:44:16 +0800

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,6 @@ interface AboutProps {
 
 const About = ({ setCurrentPage }: AboutProps) => {
   const [showFullJourney, setShowFullJourney] = useState(false);
-
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       {/* Header */}
