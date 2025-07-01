47ab1b5f27a8613a783d8e74c48bc6a1e60ddf7c
soichu <info@soichu.com>
2025-07-02 00:06:43 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -88,8 +88,9 @@ const Home = ({ setCurrentPage }) => (
       </h2>
       <div className="flex justify-center mb-4">
         <button
-onClick={() => window.location.href = '/PuebloBridge'}          className="px-8 py-3 bg-soichu-600 text-white rounded-lg shadow-lg font-light flex items-center space-x-2 hover:bg-soichu-700 transition"
-        >
+<button onClick={() => setCurrentPage('community')}>
+    className="px-8 py-3 bg-soichu-600 text-white rounded-lg shadow-lg font-light flex items-center space-x-2 hover:bg-soichu-700 transition"
+      >
           <Users className="h-5 w-5" />
           <span>Join Free Community</span>
         </button>
