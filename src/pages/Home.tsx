d78d9ba3909fa62c1aea6557391bdf86656a724b
soichu <info@soichu.com>
2025-08-29 14:01:54 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -55,12 +55,11 @@ const resources = [
     title: 'Habit Tracker',
     description: 'Printable & digital templates for growth.',
     icon: Sparkles,
-    onClick: () =>
-      window.open(
-        'https://marble-trampoline-732.notion.site/Personal-Habit-Tracker-Soichu-2574d86f03c1805885abc1c5e046efff?source=copy_link',
-        '_blank'
-      ),
-    cta: 'Download',
+    onClick: () => window.open(
+'https://marble-trampoline-732.notion.site/25e4d86f03c180ec91cafd9f643dd7e4?v=25e4d86f03c18149bee0000c9b403016&pvs=73',
+      '_blank'
+    ),
+    cta: 'Access',
   },
 ];
 
